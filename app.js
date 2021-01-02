ce8ca3d27028efb7544cfb8a3278e680b2c9b655
menefrego15 <menefrego.packm@gmail.com>
2021-01-02 20:24:03 +0100

    Update app.js

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ class People {
   
   }
 
-  var person1 = new People("Elon Musk","140000000000","https://mobile-img.lpcdn.ca/lpca/924x/r3996/3d08f534-3d2d-11ea-82bb-0eda3a42da3c.jpg");
+  var person1 = new People("Elon Musk","155000000000","https://mobile-img.lpcdn.ca/lpca/924x/r3996/3d08f534-3d2d-11ea-82bb-0eda3a42da3c.jpg");
   var person2 = new People("Post Malone","26000000","https://images3.alphacoders.com/104/1041797.jpg");
   var person3 = new People("6ix9ine","8000000","https://www.hiersoiraparis.com/wp-content/uploads/2020/05/6ix9ine-Gooba.jpg");
   var person4 = new People("Future","40000000","https://i.pinimg.com/originals/04/1a/9c/041a9c299dc8fe60304c50a8e4a8f177.png");
@@ -62,14 +62,42 @@ class People {
   var person36 = new People("Eminem","230000000","https://hiphopcorner.fr/wp-content/uploads/2020/06/eminem-goat-list-rappeurs-preferes.jpg");
   var person37 = new People("Dr Dre","820000000","https://cdn-www.konbini.com/fr/images/files/2018/05/dr-dre-dr.jpg?webp=");
   var person38 = new People("Jay Z","1000000000","https://metrodakar.com/wp-content/uploads/2020/03/jay-z.jpg.webp");
-  var person39 = new People("Kanye West","3200000000","https://mobile-img.lpcdn.ca/lpca/924x/f69d547e/b908d318-c14d-11ea-b8ad-02fe89184577.jpg");
-  
+  var person39 = new People("Kanye West","3200000000","https://static01.nyt.com/images/2020/07/16/us/politics/16kanye-fec1/merlin_164580843_dd3be151-7315-4856-8f8f-768515b175b2-articleLarge.jpg?quality=75&auto=webp&disable=upscale");
+  var person40 = new People("Bill Gates","120000000000","https://s.france24.com/media/display/3dcd5532-0fc3-11e9-ad1b-005056bff430/w:1280/p:16x9/2e87618428a5d00241fb214d78296367.webp");
+  var person41 = new People("Jeff Bezos","182000000000","https://i.insider.com/5db05a564af90978482d15b2?width=1100&format=jpeg&auto=webp");
+  var person42 = new People("Warren Buffet","87500000000","https://static01.nyt.com/images/2020/02/22/business/22berkshire1/merlin_154341555_6545c48c-70e0-45b4-a5f8-fc3ed829cd16-articleLarge.jpg?quality=75&auto=webp&disable=upscale");
+  var person43 = new People("Bernard Arnault","151000000000","https://i.insider.com/5c33d5e30df17602c7579874?width=1100&format=jpeg&auto=webp");
+  var person44 = new People("Mark Zuckerberg","99000000000","https://i.insider.com/5138bcd7eab8ea7337000013?width=750&format=jpeg&auto=webp");
+  var person45 = new People("Larry Ellison","88000000000","https://i.insider.com/5f5253e8e6ff30001d4e71a9?width=1100&format=jpeg&auto=webp");
+  var person46 = new People("Larry Page","77000000000","https://i.insider.com/560e9a599dd7cc14008bed14?width=1100&format=jpeg&auto=webp");
+  var person47 = new People("Serguei Brin","75000000000","https://i.insider.com/59cbc8e9c68d7b6d118b4922?width=1100&format=jpeg&auto=webp");
+  var person48 = new People("Jack Ma","48000000000","https://s.rfi.fr/media/display/a58364ba-10ca-11ea-b429-005056bf7c53/w:1280/p:16x9/2019-04-12t113925z_1798692368_rc1cc19432d0_rtrmadp_3_china-tech-labour_0.webp");
+  var person48 = new People("Oprah Winfrey","3500000000","https://s.france24.com/media/display/bcbf78e8-0fc3-11e9-b57f-005056a964fe/w:1280/p:16x9/9263bfed83667c1db3267d28f0d157be.webp");
+  var person49 = new People("Tom Cruise","600000000","https://cdn-www.konbini.com/fr/images/files/2020/09/tom-cruise.jpg?webp=");
+  var person50 = new People("George Clooney","500000000","https://s.france24.com/media/display/53983b54-0f4e-11e9-a160-005056a964fe/w:1280/p:16x9/george_clooney_satellite.webp");
+  var person51 = new People("Robert De Niro","500000000","https://scontent-rtl.akamaized.net/GED/06180000/6188800/6188819_700x0.webp");
+  var person52 = new People("Mel Gibson","425000000","https://scontent-rtl.akamaized.net/GED/01090000/1090500/1090515_700x0.webp");
+  var person53 = new People("Arnold Schwarzenegger","400000000","https://akns-images.eonline.com/eol_images/Entire_Site/2018016/rs_600x600-180116093750-600.Arnold-Schwarzenegger-JR-011618.jpg?fit=around%7C700:700&output-quality=90&crop=700:700;center,top");
+  var person54 = new People("Jackie Chan","400000000","https://s.rfi.fr/media/display/a58364ba-10ca-11ea-b429-005056bf7c53/w:1280/p:16x9/2019-04-12t113925z_1798692368_rc1cc19432d0_rtrmadp_3_china-tech-labour_0.webp");
+  var person55 = new People("Sylvester Stallone","400000000","https://cdn-biiinge.konbini.com/images/files/2019/05/cobra-feat.jpg?webp=");
+  var person56 = new People("Tom Hanks","400000000","https://scontent-rtl.akamaized.net/GED/09360000/9362900/9362953_700x0.webp");
+  var person57 = new People("Keanu Reeves","360000000","https://happymag.tv/wp-content/uploads/2020/06/Webp.net-resizeimage-1-5.jpg");
+  var person58 = new People("Will Smith","450000000","https://cdn-www.konbini.com/fr/images/files/2018/09/suicide-squad-pourquoi-will-smith-n-apos.jpg?webp=");
+  var person59 = new People("Brad Pitt","300000000","https://scontent-rtl.akamaized.net/GED/09060000/9066600/9066611_700x0.webp");
+  var person60 = new People("Mark Wahlberg","300000000","https://scontent-rtl.akamaized.net/GED/05070000/5079700/5079717_700x0.webp");
+  var person61 = new People("Robert Downey Jr","300000000","https://cdn-www.konbini.com/fr/images/files/2016/04/downey-feat.jpg?webp=");
+  var person62 = new People("Leonardo DiCaprio","260000000","https://www.gannett-cdn.com/presto/2019/07/24/USAT/15bd61c6-2d43-49f4-adf6-cedf2625be2d-GTY_1163685538.JPG?crop=4199,2362,x0,y166&width=660&height=372&format=pjpg&auto=webp");
+  var person63 = new People("Bruce Willis","250000000","https://i.insider.com/55dd15c59dd7cc14008b5ec6?width=600&format=jpeg&auto=webp");
 
+ 
 
   L = [person1, person2, person3, person4, person5, person6, person7, person8, person9, person10, 
     person11, person12, person13, person14, person15, person16, person17, person18, person19, person20, 
     person21, person22, person23, person24, person25, person26, person27, person28, person29, person30, 
-    person31, person32, person33, person34, person35, person36, person37, person38, person39];
+    person31, person32, person33, person34, person35, person36, person37, person38, person39, person40, 
+    person41, person42, person43, person44, person45, person46, person47, person48, person49, person50, person51, person52, 
+    person53, person54, person55, person56, person57, person58, person59, person60, person61, person62, person63,
+];
 
 //GENERATE RANDOM NUMBER AND PICK 2 PPL IN THE L TAB
 //generate random x
@@ -82,13 +110,17 @@ function randomN(L) {
 function randomy() {
     
     var y = randomN(L);
-
+/*
     if (x == y) {
         if (x == L.length -1) {
             y--;
         }else{
             y++;
         }
+    }
+*/  
+    while (x == y) {
+        y = randomN(L);
     }
     return y;
 }
@@ -363,9 +395,3 @@ if (lelangage === "french") {
 if (lelangage !== "french" && lelangage != "english") {
     setTimeout(togglePopup, 2000);
 }
-
-
-
-
-
-
